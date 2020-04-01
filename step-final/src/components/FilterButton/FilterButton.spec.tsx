25518dd0158c3d0a0d9e55bc332eca19c3c8c20f
deokgoo <kkddgg1001@gmail.com>
2020-04-01 17:08:35 +0900

    cosmetic change

--- step-final/src/components/FilterButton/FilterButton.spec.tsx
+++ step-final/src/components/FilterButton/FilterButton.spec.tsx
@@ -2,74 +2,75 @@ import React from 'react';
 import { render, unmountComponentAtNode } from "react-dom";
 import FilterButton from './index';
 import { act } from "react-dom/test-utils";
-import {filterItem, filterType} from "../../redux/types/filterType";
+import { filterItem, filterType } from "../../redux/types/filterType";
 
 let container:HTMLElement|null = null;
 
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
+describe("FilterButton UT", ()=> {
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
 
-afterEach(() => {
-  if(container===null) return;
-  unmountComponentAtNode(container);
-  container?.remove();
-  container = null;
-});
+  afterEach(() => {
+    if(container===null) return;
+    unmountComponentAtNode(container);
+    container?.remove();
+    container = null;
+  });
 
-it("visible children", () => {
-  const testChildren:filterItem = 'ALL';
-  const filterItem:filterItem = 'ALL';
-  const renderElement = (
-    <FilterButton filterItem={filterItem} _onClick={(x: filterType)=>{}}>
-      {testChildren}
-    </FilterButton>
-  );
+  it("visible children", () => {
+    const testChildren:filterItem = 'ALL';
+    const filterItem:filterItem = 'ALL';
+    const renderElement = (
+      <FilterButton filterItem={filterItem} _onClick={(x: filterType)=>{}}>
+        {testChildren}
+      </FilterButton>
+    );
 
-  act(() => { render(renderElement, container); });
+    act(() => { render(renderElement, container); });
 
-  expect(document.querySelector('button')?.textContent).toBe(testChildren);
-});
+    expect(document.querySelector('button')?.textContent).toBe(testChildren);
+  });
 
-it("button Click", () => {
-  const testChildren:filterItem = "ACTIVE";
-  const filterItem:filterItem = 'ALL';
-  const _onClickMock = jest.fn();
+  it("button Click", () => {
+    const testChildren:filterItem = "ACTIVE";
+    const filterItem:filterItem = 'ALL';
+    const _onClickMock = jest.fn();
 
-  const renderElement = (
-    <FilterButton filterItem={filterItem} _onClick={_onClickMock}>
-      {testChildren}
-    </FilterButton>
-  );
+    const renderElement = (
+      <FilterButton filterItem={filterItem} _onClick={_onClickMock}>
+        {testChildren}
+      </FilterButton>
+    );
 
-  act(() => {
-    render(renderElement, container);
+    act(() => {
+      render(renderElement, container);
+      let btn = document.querySelector('button');
+      btn?.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    });
+
+    expect(_onClickMock).toHaveBeenCalledTimes(1);
+    expect(_onClickMock).toHaveBeenCalledWith({filter: testChildren});
+  });
+
+  it("filtered Btn", () => {
+    const testChildren:filterItem = "ALL";
+    const filterItem:filterItem = 'ALL';
+
+    const renderElement = (
+      <FilterButton filterItem={filterItem} _onClick={(x: filterType)=>{}}>
+        {testChildren}
+      </FilterButton>
+    );
+
+    act(() => {
+      render(renderElement, container);
+    });
+
     let btn = document.querySelector('button');
-    btn?.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+
+    expect(btn?.classList).toContain('selected');
+
   });
-
-  expect(_onClickMock).toHaveBeenCalledTimes(1);
-  expect(_onClickMock).toHaveBeenCalledWith({filter: testChildren});
-});
-
-it("filtered Btn", () => {
-  const testChildren:filterItem = "ALL";
-  const filterItem:filterItem = 'ALL';
-  const _onClickMock = jest.fn();
-
-  const renderElement = (
-    <FilterButton filterItem={filterItem} _onClick={(x: filterType)=>{}}>
-      {testChildren}
-    </FilterButton>
-  );
-
-  act(() => {
-    render(renderElement, container);
-  });
-
-  let btn = document.querySelector('button');
-
-  expect(btn?.classList).toContain('selected');
-
 });
